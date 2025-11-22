e21124ad49b622ffc40ac8c2b39efbcff4c7d455
jaytea2 <allenfrncisco@gmail.com>
2025-11-23 06:29:12 +0800

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -2,5 +2,6 @@
 document.querySelector('.apply-link').addEventListener('click', function() 
 {
     alert("Redirecting to application page...");
-    window.location.href = "../form/form.html";
+    window.location.href = "form/form.html";
+
 });
