5b24f325d564d98acb8259569ba1846785fdc450
jaytea2 <allenfrncisco@gmail.com>
2025-11-23 05:46:04 +0800

    Add files via upload

---
+++ mainscript.js
@@ -0,0 +1,6 @@
+
+document.querySelector('.apply-link').addEventListener('click', function() 
+{
+    alert("Redirecting to application page...");
+    window.location.href = "../form/form.html";
+});
